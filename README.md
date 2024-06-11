350b564e372c8e9e55dce62db2d53de0a0849f84
halka <halka@zunda.co.jp>
2024-06-11 12:59:46 +0900

    リファクタとかした

---
+++ README.md
@@ -0,0 +1,19 @@
+# SMS Transfer
+Relay SMS to Slack.
+## Equipment
+- Raspberry Pi (4B+)
+- [Soracom Onyx - LTE™ USB ドングル (SC-QGLC4-C1) - IoTデバイス通販 - SORACOM (ソラコム) IoTストア](https://soracom.jp/store/7326/)
+- nano SIM
+## Prepare
+- Install Depended Packages
+- Setting Cellular Network by Network Manager and Modem Manager
+- Copy, Rename and Edit `config.json`
+- Connect Dongle to Raspberry Pi
+- Start Service
+- Test
+- Enable Service
+## Special Thanks
+- [株式会社システムゼウス](https://www.systemzeus.co.jp/)
+  - [SystemzeusInc/SMSForwardingBot: SMSを受信しSlackにデータを送信するPythonツール](https://github.com/SystemzeusInc/SMSForwardingBot) 
+## License
+This projects is licensed under the MIT License, see the [LICENSE.txt](/LICENSE.txt) file for details.
--- transfer.py
+++ transfer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import os
 import time
 
 from slack_sdk import WebClient
@@ -10,10 +11,10 @@ gi.require_version('ModemManager', '1.0')
 from gi.repository import Gio, ModemManager
 
 
-with open('/home/shirakobato/sms2slack/config.json') as f:
+with open(os.path.join(os.path.dirname(__file__), 'config.json') as f:
     config = json.load(f)
 
-def transfer_to_slack(text):
+def post_to_slack(text):
     client = WebClient(token=config['slack']['token'])
     try:
         response = client.chat_postMessage(channel=config['slack']['channel'], text=text)
@@ -27,21 +28,28 @@ def transfer_to_slack(text):
         assert isinstance(e.response.status_code, int)
         print(f"Received a response status_code: {e.response.status_code}")
 
-def get_sms():
-    mngr = ModemManager.Manager.new_sync(
-        Gio.bus_get_sync(Gio.BusType.SYSTEM, None),
-        Gio.DBusObjectManagerClientFlags.DO_NOT_AUTO_START,
-        None)
-    obj = mngr.get_objects()[0]
-    msg = obj.get_modem_messaging()
-    recieved = msg.list_sync()
-    for m in recieved:
-        print(m.get_number(), m.get_timestamp(), '"%s"' % m.get_text())
-        transfer_to_slack('From: ' + m.get_number() + "\n" + m.get_text())
-        msg.delete_sync(m.get_path())
-
-if __name__ == '__main__':
+def transfer():
     while True:
-        get_sms()
+        # sync dongle
+        manager = ModemManager.Manager.new_sync(
+            Gio.bus_get_sync(Gio.BusType.SYSTEM, None),
+            Gio.DBusObjectManagerClientFlags.DO_NOT_AUTO_START, None)
+        manager_objects = manager.get_objects()
+        
+        # check messages
+        for manager_object in manager_objects:
+            messaging = manager_object.get_modem_messaging()
+            messages = messaging.list_sync()
+            
+            # post to slack each messages
+            for message in messages:
+                post_to_slack(f"From: {message.get_number()}\n{message.get_text()}")
+                # delete message from dongle or sim
+                messaging.delete_sync(message.get_path())
+        
+        # interval defined in config.json 
         time.sleep(config['general']['interval'])
 
+# start
+exit(transfer())
+        
