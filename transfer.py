9c869cb84208c00bc737d593ff19fd7d94365635
halka <halka@rjch.jp>
2025-08-20 18:52:03 +0900

    formatted.

--- transfer.py
+++ transfer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from gi.repository import Gio, ModemManager
 import json
 import os
 import time
@@ -8,25 +9,28 @@ from slack_sdk.errors import SlackApiError
 
 import gi
 gi.require_version('ModemManager', '1.0')
-from gi.repository import Gio, ModemManager
 
 
 with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
     config = json.load(f)
 
+
 def post_to_slack(text):
     client = WebClient(token=config['slack']['token'])
     try:
-        response = client.chat_postMessage(channel=config['slack']['channel'], text=text)
+        response = client.chat_postMessage(
+            channel=config['slack']['channel'], text=text)
         # assert response["message"]["text"] == "Hello world!"
     except SlackApiError as e:
         # You will get a SlackApiError if "ok" is False
         assert e.response["ok"] is False
-        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        # str like 'invalid_auth', 'channel_not_found'
+        assert e.response["error"]
         print(f"Got an error: {e.response['error']}")
         # Also receive a corresponding status_code
         assert isinstance(e.response.status_code, int)
         print(f"Received a response status_code: {e.response.status_code}")
+
 
 def transfer():
     # sync dongle
@@ -38,12 +42,12 @@ def transfer():
         # check messages
         messaging = manager_object.get_modem_messaging()
         messages = messaging.list_sync()
-          
         # post to slack each messages
         for message in messages:
             post_to_slack(f"{message.get_number()}\n{message.get_text()}")
             # delete message from dongle or sim
             messaging.delete_sync(message.get_path())
+
 
 # start
 if __name__ == '__main__':
